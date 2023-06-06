e335eb856780a287254d63e1ecdf8c838e50da4e
Fernando Lorenzo Ramírez <fernandolorenzo710@gmail.com>
2023-06-05 20:35:22 -0600

    adding api key

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'API_KEY_HERE',
+        api_key: '3d97f4174127a4ec94aeb9586798777c',
         language: 'es-ES'
     }
 });
